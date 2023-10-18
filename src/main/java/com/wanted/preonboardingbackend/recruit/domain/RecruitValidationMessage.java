c9436460044c7ae77da3d858188e11ef09eb716f
kade <bhpark93@gmail.com>
2023-10-18 22:58:08 +0900

    add : 채용공고 지원 validation 추가

--- src/main/java/com/wanted/preonboardingbackend/recruit/domain/RecruitValidationMessage.java
+++ src/main/java/com/wanted/preonboardingbackend/recruit/domain/RecruitValidationMessage.java
@@ -1,6 +1,7 @@
 package com.wanted.preonboardingbackend.recruit.domain;
 
 public class RecruitValidationMessage {
+    public static final String RECRUIT_ID_BLANK = "채용공고가 없습니다.";
     public static final String JOB_POSITION_BLANK = "채용 포지션을 입력해주세요.";
     public static final String JOB_DESCRIPTION_BLANK = "채용 내용을 입력해주세요.";
     public static final String REQUIRED_SKILLS_BLANK = "사용 기술을 입력해주세요";
--- src/main/java/com/wanted/preonboardingbackend/recruit/dto/RecruitApplyRequest.java
+++ src/main/java/com/wanted/preonboardingbackend/recruit/dto/RecruitApplyRequest.java
@@ -1,5 +1,6 @@
 package com.wanted.preonboardingbackend.recruit.dto;
 
+import com.wanted.preonboardingbackend.recruit.domain.RecruitValidationMessage;
 import com.wanted.preonboardingbackend.user.domain.UserValidationMessage;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class RecruitApplyRequest {
+    @NotBlank(message= RecruitValidationMessage.RECRUIT_ID_BLANK)
     String recruitId;
 
     @NotBlank(message= UserValidationMessage.USER_ID_BLANK)
